e8bce5784cbabe0ebd7395af9a852912dd6d97ad
Vidur Chanana <vidurchanana@hotmail.com>
2017-08-25 19:10:37 -0700

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,19 +1,17 @@
-package org.HackerRank.Token;
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map.Entry;
-import java.util.PriorityQueue;
+
 
 public class Solution {
 	
 	public String electionWinner(String[] votes){
 		
 		HashMap<String, Integer> map = new HashMap<>();
-		int max=0;
+		LinkedList<String> finalNames = new LinkedList<>(); 
+		int max = 0;
+		
 		for(int i = 0; i < votes.length; i++){
 			String s = votes[i];
 			s = s.trim();
@@ -24,24 +22,19 @@ public class Solution {
 				map.put(s, 1);
 			}
 			max=Math.max(max, map.get(s));
+		}	
+		
+		for (Entry<String, Integer> entry : map.entrySet());
+		    if (entry.getValue()==max) 
+			finalNames.add(entry.getKey());
 		}
-		
-		int j = 0;
-		LinkedList<String> finalNames = new LinkedList<>(); 
-		
-        for (Entry<String, Integer> entry : map.entrySet()) {
-        	System.out.println(entry.getKey());
-        	System.out.println(entry.getValue());
-            if (entry.getValue()==max) 
-            	finalNames.add(entry.getKey());
-        }
-        Collections.sort(finalNames);
-        return finalNames.get(finalNames.size()-1);
+		Collections.sort(finalNames);
+		return finalNames.get(finalNames.size()-1);
 	}
        
 	
 	public static void main(String[] args){
-		String[] votes = {"Vidur", "Vidur", "Vidur", "Vidur", "Z", "Bhumika", "Bhumika", "Bhumika", "Bhumika"};
+		String[] votes = {"A", "A", "A", "A", "Z", "B", "B", "B", "B"};
 		System.out.println(new Solution().electionWinner(votes));
 	}
 }
